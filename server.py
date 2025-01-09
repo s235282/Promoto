b4ed336970cde8cc0b69a259ec6b057504e2810c
s235282 <s235282@dtu.dk>
2025-01-09 21:24:34 +0100

    Removed some return message

--- server.py
+++ server.py
@@ -41,7 +41,6 @@ def chat():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
-        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
